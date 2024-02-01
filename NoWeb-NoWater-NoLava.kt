922d5e59d8e546be19297a98af736b1c44f6ef68
Pursue <139882428+FKtzs@users.noreply.github.com>
2024-02-01 18:58:00 +0800

    Update NoWeb-NoWater-NoLava.kt
    移除岩浆、水、蜘蛛网的减速

--- NoWeb-NoWater-NoLava.kt
+++ NoWeb-NoWater-NoLava.kt
@@ -1,8 +1,3 @@
-/*
- * LiquidBounce Hacked Client
- * A free open source mixin-based injection hacked client for Minecraft using Minecraft Forge.
- * https://github.com/CCBlueX/LiquidBounce/
- */
 package net.ccbluex.liquidbounce.features.module.modules.movement
 
 import net.ccbluex.liquidbounce.event.EventTarget
@@ -21,7 +16,7 @@ import net.minecraft.init.Blocks
 import net.minecraft.network.play.client.CPacketPlayerDigging
 import net.minecraft.util.EnumFacing
 
-@ModuleInfo(name = "NoWeb-NoWater-NoLava", description = "Prevents you from getting slowed down in webs.", category = ModuleCategory.MOVEMENT)
+@ModuleInfo(name = "NoWeb-NoWater-NoLava", description = "noWeb,water,lava slow", category = ModuleCategory.MOVEMENT)
 class NoWeb : Module() {
 
     @EventTarget
